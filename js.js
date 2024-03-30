c98542238d257440721740faa807ade98657c53c
Godric <godric@korh.cz>
2024-03-30 15:39:51 +0100

    exclude localhost

--- js.js
+++ js.js
@@ -3,6 +3,10 @@
     const target = src.slice(0, src.lastIndexOf('/')) + '/record.php'
     const rn = () => Math.floor(1e8 + Math.random() * (1e9 - 1e8)) + ''
 
+    if (['localhost', '127.0.0.1'].includes(document.location.hostname)) {
+        return
+    }
+
     let uid = window.localStorage.getItem('stat_uid')
     if (uid === null) {
         uid = rn() + rn()
